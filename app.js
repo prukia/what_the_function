8ca3f512a5fb14c16be6623994072f8f23574f1a
Brian Schorn <schorn.brian@gmail.com>
2017-01-06 10:33:44 -0600

    Added the code to execute per instructions

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ function Student(firstName, favoriteColor) {
  * 1. Use the Student constructor function to create variables
  * that represent you and your coding partner(s).
  */
+var brian = new Student("Brian", "Red");
+var rukia = new Student("Rukia", "Purple");
 
 /**
   * 2. Create a function that takes one argument. That argument will
   * represent a single student object. The function will log the student's
   * name.
   */
-
+function printName(student){
+  console.log("This students first name is: " + student.firstName);
+}
 /***** SWITCH PROGRAMMERS *****/
 
 /**
@@ -21,14 +25,20 @@ function Student(firstName, favoriteColor) {
   * represent a single student object. The function will log the student's
   * favoriteColor.
   */
-
+function printColor(color) {
+  console.log("This students favorite color is: " + color.favoriteColor);
+}
 /**
   * 4. Put both students created in Step 1 into an array called students.
   */
-
+var studentArray = [brian, rukia];
 /***** SWITCH PROGRAMMERS *****/
 
 /**
  * 5. Loop over the students array and invoke both functions from Step 2 and
  * Step 3. You should see console logs for each students firstName and favoriteColor.
  */
+for(i=0; i<studentArray.length; i++){
+  printName(studentArray[i]);
+  printColor(studentArray[i]);
+}
