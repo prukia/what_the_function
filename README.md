6929140adbda50507e6cf302e09a2318ada09986
Antoinette Smith <antoinette@primeacademy.io>
2017-01-06 10:01:50 -0600

    added base assignment files

---
+++ README.md
@@ -0,0 +1,14 @@
+# What the Function
+
+Let's reinforce what we know about functions and objects!
+
+Follow the comments in `app.js` and note when to switch from driver
+to navigator/observer and back again.
+
+## Helpful Tips
+
+* When appropriate add console logs after each step to see what your
+data looks like.
+* Write your code under the comment that is associated with it.
+* Open the `index.html` file in a browser and reload it every time you update and
+save your JavaScript file.
---
+++ app.js
@@ -0,0 +1,34 @@
+function Student(firstName, favoriteColor) {
+  this.firstName = firstName;
+  this.favoriteColor = favoriteColor;
+}
+
+/**
+ * 1. Use the Student constructor function to create variables
+ * that represent you and your coding partner(s).
+ */
+
+/**
+  * 2. Create a function that takes one argument. That argument will
+  * represent a single student object. The function will log the student's
+  * name.
+  */
+
+/***** SWITCH PROGRAMMERS *****/
+
+/**
+  * 3. Create a function that takes one argument. That argument will
+  * represent a single student object. The function will log the student's
+  * favoriteColor.
+  */
+
+/**
+  * 4. Put both students created in Step 1 into an array called students.
+  */
+
+/***** SWITCH PROGRAMMERS *****/
+
+/**
+ * 5. Loop over the students array and invoke both functions from Step 2 and
+ * Step 3. You should see console logs for each students firstName and favoriteColor.
+ */
